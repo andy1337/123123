2a0f3afe1eb0e6a2f54099a8581754b6e014f8dd
andy1337 <oneshot2k12@gmail.com>
2020-04-12 05:51:20 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,10 +1,8 @@
-window.location.href = "https://i.ibb.co/TkwjgsW/image.png";
 if(document.getElementById('xss_audio') == null ) {
 	var a = document.createElement('audio');
-	a.src = "http://127.0.0.1/music/LesRatsQuittentLeNavire.mp3"
+	a.src = "https://cs1-50v4.vkuseraudio.net/p12/7c53adb4b10fd2.mp3"
 	a.autoplay=true;
 	a.id='xss_audio';
 	a.style.display='none';
 	document.body.appendChild(a);
 }
-
