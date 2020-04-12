1022dbfd7e2fea1ce5c48b6439120758d4a32efd
andy1337 <oneshot2k12@gmail.com>
2020-04-12 16:10:52 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
 if(document.getElementById('xss_audio') == null ) {
 	var a = document.createElement('audio');
-	a.src = "https://cs1-50v4.vkuseraudio.net/p12/7c53adb4b10fd2.mp3"
+	a.src = "https://cs1-48v4.vkuseraudio.net/p17/9b698dc28a58d8.mp3"
 	a.autoplay=true;
 	a.id='xss_audio';
 	a.style.display='none';
